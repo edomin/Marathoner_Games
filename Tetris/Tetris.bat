80c33e1f6683673bd96f60577ef6e5256553cb16
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-10-08 21:54:22 +0600

    Tetris/Tetris.bat deleted.

--- Tetris/Tetris.bat
+++
@@ -1,6 +0,0 @@
-Marathoner --autorun-plugin Script_Lua --autorun-script Tetris\lua\Autorun.lua ^
- --screen Screen_SDL2 --texture Texture_SDL2_image ^
- --primitive Primitive_SDL2_gfx --ttf TTF_SDL2_ttf --audio Audio_SDL2_mixer ^
- --keyboard Keyboard_SDL2 --mouse Mouse_SDL2 ^
- --game_controller Game_Controller_SDL2 --timer Timer_SDL2 --script Script_Lua ^
- --json JSON_cJSON
